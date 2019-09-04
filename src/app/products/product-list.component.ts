1d7f2e7e7dc8f174f4eb465022bb01a4e025c8a8
Keerthivasan RJ <keerthikalai02@gmail.com>
2019-09-04 22:53:13 +0530

    HomeWork sep-4

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -73,7 +73,9 @@ export class ProductListComponent implements OnInit{
                 "imageUrl": "assets/images/xbox-controller.png"
               }
     ];
-
+    onRatingClicked(message: string): void{
+      this.pageTitle = 'Product List: ' + message;
+    }
     toggleImage() : void{
         this.showImage = !this.showImage;
     }
